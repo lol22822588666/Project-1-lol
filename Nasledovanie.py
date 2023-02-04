40050093c52a89453892685854341eadc397d7d2
lol22822588666 <vivivilka202@gmail.com>
2023-02-04 17:50:23 +0200

    initial commit

--- Nasledovanie.py
+++ Nasledovanie.py
@@ -6,14 +6,23 @@ class Phone:
     def call(self):
         if self.isWorking:
             print("Вам звонят")
-class Mobile(Phone):
+    def info(self):
+        print("Вкл", self.isWorking)
+class PhoneMobile:
         def __init__(self):
             super().__init__()
             self.battery = 0
         def charge(self, procents):
             self.battery = procents
             print("Заряд: ", self.battery, "%")
-tel1 = Mobile()
-tel1.work()
-tel1.call()
-tel1.charge(59)
+        def info(self):
+            print("Вкл", self.isWorking)
+    def poli():
+        for i in Phone, PhoneMobile:
+            i().info()
+print(poli())
+
+#tel1 = Mobile()
+#tel1.work()
+#tel1.call()
+#tel1.charge(59)
